8a67cac6199dc993e7db18632b9e90424e5104b1
Kevin Falcetano <kevin.falcetano@gmail.com>
2026-01-14 20:56:22 -0800

    more updates

--- init.lua
+++ init.lua
@@ -1,17 +1,10 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 vim.o.laststatus = 3
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 vim.wo.number = true
-
--- In your persistence.nvim config or init.lua
-vim.api.nvim_create_autocmd('User', {
-  pattern = 'PersistenceSavePre',
-  callback = function()
-    -- Force close Neo-tree before saving the session
-    vim.cmd('Neotree close')
-  end,
-})
 
 require('config.lazy')
 
