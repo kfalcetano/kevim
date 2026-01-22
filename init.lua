48ba99860db34dcb77440b462b6f45bd8d26db14
Kevin Falcetano <kevin.falcetano@gmail.com>
2026-01-21 18:50:48 -0800

    treesitter config

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
-vim.opt.softtabstop = 4
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.foldenable = false
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
@@ -13,7 +14,19 @@ require('config.lazy')
 
 vim.cmd('colorscheme carbonfox')
 
+require('nvim-treesitter.install').compilers = { 'zig', 'gcc', 'clang' }
 require('nvim-treesitter').install({ 'python', 'lua' })
+
+-- Autocmds to enable treesitter functions on filetypes
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'python', 'lua' },
+  callback = function()
+    vim.treesitter.start()
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
 
 -- Close the current buffer without messing with the layout
 vim.keymap.set('n', '<leader>bd', function()
