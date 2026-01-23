11dae2a8d4f23c9c04f470a9caa7975bfb000665
Kevin Falcetano <kevin.falcetano@gmail.com>
2026-01-22 17:58:05 -0800

    update neotree staged symbol

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,58 +1,56 @@
 return {
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    lazy = false,
-    branch = 'v3.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
-      'MunifTanjim/nui.nvim',
+  'nvim-neo-tree/neo-tree.nvim',
+  lazy = false,
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  cmd = 'Neotree',
+  keys = {
+    { '<leader>gs', '<cmd>Neotree toggle git_status left<cr>', desc = 'Git Status' },
+    { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'Toggle Neo-tree' },
+    { '<leader>o', '<cmd>Neotree focus<cr>', desc = 'Focus Neo-tree' },
+  },
+  opts = {
+    window = {
+      width = 30,
     },
-    cmd = 'Neotree',
-    keys = {
-      { '<leader>gs', '<cmd>Neotree toggle git_status left<cr>', desc = 'Git Status' },
-      { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'Toggle Neo-tree' },
-      { '<leader>o', '<cmd>Neotree focus<cr>', desc = 'Focus Neo-tree' },
+    open_on_setup = false,
+    ignore_ft_on_setup = {
+      'startify',
+      'dashboard',
+      'alpha',
+      'Outline',
+      'spectre_panel',
+      'toggleterm',
+      'qf',
+      'TelescopePrompt',
     },
-    opts = {
-      window = {
-        width = 30,
+    hijack_netrw_behavior = 'open_default',
+    filesystem = {
+      use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true,
       },
-      open_on_setup = false,
-      ignore_ft_on_setup = {
-        'startify',
-        'dashboard',
-        'alpha',
-        'Outline',
-        'spectre_panel',
-        'toggleterm',
-        'qf',
-        'TelescopePrompt',
-      },
-      hijack_netrw_behavior = 'open_default',
-      filesystem = {
-        use_libuv_file_watcher = true,
-        filtered_items = {
-          visible = true,
-        },
-        follow_current_file = { enabled = true },
-        close_if_last_window = false,
-      },
-      -- This is the Git integration part
-      default_component_configs = {
-        git_status = {
-          symbols = {
-            -- Change these symbols to your liking
-            added = 'A',
-            modified = 'M',
-            deleted = 'D',
-            renamed = 'R',
-            untracked = 'U',
-            ignored = '',
-            unstaged = '',
-            staged = '✔',
-            conflict = '',
-          },
+      follow_current_file = { enabled = true },
+      close_if_last_window = false,
+    },
+    -- This is the Git integration part
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          -- Change these symbols to your liking
+          added = 'A',
+          modified = 'M',
+          deleted = 'D',
+          renamed = 'R',
+          untracked = 'U',
+          ignored = '',
+          unstaged = '',
+          staged = '✅',
+          conflict = '',
         },
       },
     },
