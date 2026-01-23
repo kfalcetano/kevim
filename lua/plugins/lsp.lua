7c02425f3ca2486b36f60f053392e126fd96b6a9
Kevin Falcetano <kevin.falcetano@gmail.com>
2026-01-22 17:34:34 -0800

    lua-specific intents and goto  definition keybinds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -47,9 +47,30 @@ return {
         vim.lsp.config(server, config)
         vim.lsp.enable(server)
       end
+
+      -- Keybinds
       vim.keymap.set('n', 'K', function()
         vim.lsp.buf.hover({ border = 'rounded', max_height = 25, max_width = 120 })
       end, { desc = 'Hover documentation' })
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          -- Define your keymaps or autocmds here
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf })
+          -- Open definition in a horizontal split
+          vim.keymap.set('n', 'gh', function()
+            vim.cmd('split')
+            vim.lsp.buf.definition()
+          end, { buffer = args.buf, desc = 'LSP definition in horizontal split' })
+
+          -- Open definition in a vertical split (using 'gv' or similar)
+          vim.keymap.set('n', 'gv', function()
+            vim.cmd('vsplit')
+            vim.lsp.buf.definition()
+          end, { buffer = args.buf, desc = 'LSP definition in vertical split' })
+        end,
+      })
     end,
   },
 }
