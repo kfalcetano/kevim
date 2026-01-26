9b20d03a5e40b0506365407460402ea78aea9a96
Kevin Falcetano <kevin.falcetano@gmail.com>
2026-01-25 17:39:47 -0800

    add symbol search to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,13 @@ return {
       { '<leader>fg', '<cmd>Telescope live_grep<cr>', { desc = 'Telescope live grep' } },
       { '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Telescope buffers' } },
       { '<leader>fh', '<cmd>Telescope help_tags<cr>', { desc = 'Telescope help tags' } },
+      { '<leader>fds', '<cmd>Telescope lsp_document_symbols<cr>', { desc = 'Telescope find document symbols' } },
+      { '<leader>fws', '<cmd> Telescope lsp_workspace_symbols<cr>', { desc = 'Telescope find workspace symbols' } },
+      {
+        '<leader>fdf',
+        '<cmd>Telescope lsp_document_symbols symbols=function,method<cr>',
+        { desc = 'Telescope find document functions and methods' },
+      },
     },
   },
 }
