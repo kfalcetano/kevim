6c8c6b4d7c14a3bc73021c40f9cd4467a540d306
Kevin Falcetano <kevin.falcetano@gmail.com>
2026-02-23 21:33:24 -0800

    add git reset hunk to visual mode

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -16,6 +16,6 @@ return {
     { ']c', ':Gitsigns nav_hunk next<cr>', desc = 'Next git change' },
     { '[c', ':Gitsigns nav_hunk prev<cr>', desc = 'Previous git change' },
     { '<leader>gp', ':Gitsigns preview_hunk<cr>', desc = 'Preview git change' },
-    { '<leader>gr', ':Gitsigns reset_hunk<cr>', desc = 'Reset git change / range' },
+    { '<leader>gr', ':Gitsigns reset_hunk<cr>', mode = { 'n', 'v' }, desc = 'Reset git change / range' },
   },
 }
