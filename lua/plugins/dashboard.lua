f0863ffb55c27d6e28c13d70b06e19ff6d387409
Kevin Falcetano <kevin.falcetano@gmail.com>
2026-05-04 11:54:59 -0700

    bufferline is an antipattern

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -24,7 +24,6 @@ return {
       dashboard.button('f', ' ' .. ' Find file', '<cmd>Telescope find_files<cr>'),
       dashboard.button('n', ' ' .. ' New file', [[<cmd> ene <BAR> startinsert <cr>]]),
       dashboard.button('g', ' ' .. ' Find text', '<cmd>Telescope live_grep<cr>'),
-      dashboard.button('s', ' ' .. ' Restore Session', [[<cmd> lua require("persistence").load() <cr>]]),
       dashboard.button('l', '󰒲 ' .. ' Lazy', '<cmd> Lazy <cr>'),
       dashboard.button('c', ' ' .. ' Close Dashboard', '<cmd> Alpha <cr>'),
       dashboard.button('q', ' ' .. ' Quit', '<cmd> qa <cr>'),
--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,6 +1,12 @@
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {},
+  opts = {
+    sections = {
+      lualine_c = {
+        { 'filename', path = 1 },
+      },
+    },
+  },
   lazy = false,
 }
